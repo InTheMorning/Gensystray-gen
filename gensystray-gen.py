d19092310e03eb8058930d62b487a06b15beada1
HeyCitizen <4637290+InTheMorning@users.noreply.github.com>
2021-03-18 01:50:08 -0400

    initial commit

---
+++ gensystray-gen.py
@@ -0,0 +1,195 @@
+#!/bin/python
+
+import argparse
+import configparser
+import os
+
+
+def get_arguments():
+    # returns a dict of configuration settings
+    # from commandline arguments
+
+    # parse commandline arguments
+    parser = argparse.ArgumentParser(description='Generates gensystray.cfg',
+                                     prog='gensystray-gen',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter
+                                     )
+    parser.add_argument('NAME',
+                        type=str,
+                        help='a name for the config file'
+                        )
+    parser.add_argument('-d', '--directory',
+                        type=str, metavar='<PATH>',
+                        default=os.getcwd(),
+                        help='a path to the directory to scan'
+                        )
+    parser.add_argument('-e', '--file_extension',
+                        type=str, metavar='<EXTENSION>',
+                        default=None,
+                        help='the file extension to look for'
+                        )
+    parser.add_argument('-n', '--no_systemd_update',
+                        action='store_true',
+                        help='bypasses systemd file monitor service creation'
+                        )
+    parser.add_argument('-c', '--open_command',
+                        type=str, metavar='<COMMAND>',
+                        default='xdg-open',
+                        help='command to open the files'
+                        )
+    args = parser.parse_args()
+
+    # extract dict
+    argsdict = vars(args)
+
+    # this has to be hardcoded for the time being
+    # because I don't know how to do this with systemd
+    argsdict['gensystray_root'] = '~/.config/gensystray'
+
+    # expand paths
+    for a in ['directory', 'gensystray_root']:
+        argsdict[a] = os.path.expanduser(argsdict[a])
+
+    # convert none type to empty string for extension
+    if not argsdict['file_extension']:
+        argsdict['file_extension'] = ''
+
+    return argsdict
+
+
+def get_files(arguments):
+    # Returns a dict with names and paths for files with given extension
+
+    directory = arguments['directory']
+    extension = arguments['file_extension']
+
+    # Create an empty dict
+    item_pairs = {}
+
+    # When running recursively...
+    # for root, directories, files in os.walk(directory):
+
+    # When non-recursively...
+    root = next(os.walk(directory))[0]
+    for filename in next(os.walk(directory))[2]:
+
+        if not extension:
+            # no extension specified, add file to dict
+            filepath = os.path.join(root, filename)
+            item_pairs.update({filename: filepath})
+        elif filename.endswith(extension) and len(filename) > len(extension):
+            # found valid extension, separate name and add to dict
+            filepath = os.path.join(root, filename)
+            name = filename.rsplit(extension, 1)[0]
+            item_pairs.update({name: filepath})
+        # file doesn't match, do nothing
+
+    return item_pairs
+
+
+def write_config(filedict, arguments):
+    # receives dict and write to file
+
+    config_name = arguments['NAME']
+    directory = arguments['directory']
+    extension = arguments['file_extension']
+    open_command = arguments['open_command']
+    gensystray_root = arguments['gensystray_root']
+
+    # check if dir exists, create it if needed
+    outputdir = os.path.join(gensystray_root, config_name)
+    if not os.path.isdir(outputdir):
+        os.mkdir(outputdir, 0o0700)
+
+    # the output for gensystray config file
+    outputfile = os.path.join(outputdir, 'gensystray.cfg')
+
+    # the location of the icon file
+    iconfile = os.path.join(outputdir, config_name + '.png')
+
+    # use default icon if none found
+    if not os.path.isfile(iconfile):
+        os.popen('cp /usr/share/gensystray/gensystray.png' + ' ' + iconfile)
+
+    # write the ini file for gensystray
+    with open(outputfile, 'w') as f:
+        # icon
+        f.write('@' + iconfile + '\n\n')
+
+        # tooltip
+        f.write("'" + config_name + "'" + '\n\n\n')
+
+        # all items
+        for filename, filepath in filedict.items():
+            f.write('[%s]\n%s %s\n\n' % (filename, open_command, filepath))
+
+        # [New file] section
+        # zenity command string that gets evaluated
+        zenity_cmd = ("TITLE=$(zenity --entry --text='New file name:' "
+                      "--title='Add new file' --entry-text 'Untitled') "
+                      "|| exit 1 && ")
+
+        # construct the newfile command string
+        newfile_command = (zenity_cmd + open_command + ' '
+                           + directory + '$TITLE' + extension)
+        # surround it with separators and write it
+        f.write('[%s]\n%s\n\n' % ('-', '-'))
+        f.write('[%s]\n%s\n\n' % ('New File', newfile_command))
+        f.write('[%s]\n%s\n\n' % ('-', '-'))
+
+
+def write_systemd(arguments):
+    # Make systemd .path and .service files
+
+    config_name = arguments['NAME']
+    directory = arguments['directory']
+    extension = arguments['file_extension']
+    open_command = arguments['open_command']
+    servicename = 'gensystray-refresh@'
+    pathfile_name = servicename + config_name + '.path'
+    unitfile_name = servicename + config_name + '.service'
+    unitfile_string = '%s%si.service' % (servicename, '%')
+    execstart_string = (
+                        '%sh/build/gensystray/gensystray-gen.py %s %s %s %s %s %s %s %s' %
+                        ('%', config_name, '-n', '-c', open_command,
+                         '-d', directory, '-e', extension)
+                         )
+    outputdir = os.path.expanduser('~/.config/systemd/user/')
+    output_pathfile = os.path.join(outputdir, pathfile_name)
+    output_unitfile = os.path.join(outputdir, unitfile_name)
+
+    # create the .path and .service files
+    pathfile = configparser.ConfigParser(interpolation=None)
+    pathfile.optionxform = str
+    pathfile['Unit'] = {'Description': 'Monitor a folder file for changes'}
+    pathfile['Path'] = {'PathModified': directory,
+                        'Unit': unitfile_string}
+    pathfile['Install'] = {'WantedBy': 'multi.user.target'}
+
+    unitfile = configparser.ConfigParser(interpolation=None)
+    unitfile.optionxform = str
+    unitfile['Unit'] = {'Description': 'Refreshes gensystray config after file changes'}
+    unitfile['Service'] = {'Type': 'oneshot',
+                           'ExecStart': execstart_string}
+    unitfile['Install'] = {'WantedBy': 'multi.user.target'}
+
+    # write the files in ini format
+    with open(output_pathfile, 'w') as f:
+        pathfile.write(f)
+    with open(output_unitfile, 'w') as f:
+        unitfile.write(f)
+
+    # reload and enable main .service
+    os.system('systemctl --user daemon-reload')
+    os.system('systemctl --user enable gensystray@%s.service' %
+              config_name)
+    os.system('systemctl --user start gensystray@%s.service' %
+              config_name)
+
+
+arguments = get_arguments()
+filedict = get_files(arguments)
+
+write_config(filedict, arguments)
+if not arguments['no_systemd_update']:
+    write_systemd(arguments)
---
+++ readme.md
@@ -0,0 +1,39 @@
+
+# Gensystray-gen
+
+Generates a configuration for [Gensystray - *generic system tray*](https://github.com/dardevelin/gensystray).
+
+Searches a path for files you want and add an entry in gensystray for each file, then creates/overwrites a gensystray.cfg file in a named subfolder in `~/.config/gensystray`.  The gensystray menu will also contain a *New File* entry that prompts the creation of a new entry using [Zenity](https://gitlab.gnome.org/GNOME/zenity)
+
+Unless it is run with the '-n' argument, it will also autogenerate the following files in `~/.config/systemd/user/`:
+- `gensystray@<name>.service` the systemd instance to run gensystray
+- `gensystray-refresh@<name>.path` to refresh entries in gensystray when file modifications are detected
+
+
+## Usage
+
+The systray icon can be started/stopped using
+```
+% systemctl --user start/stop gensystray@<name>
+```
+To enable this tray icon at startup, add the above `systemctl start` command to your desktop enviroment's autostart
+
+
+## Examples
+
+Make a new gensystray icon called 'notes' that shows a listing of `.md` files in the `~/Notes/` folder.
+```
+% gensystray-gen.py Notes -d ~/Notes -e .md
+```
+
+
+Using the **-c** argument to run scripts using a specific command 
+```
+% gensystray-gen.py Scripts -d ~/Scripts/ -c 'xfce4-terminal -H -e'
+```
+
+
+## ToDo
+
++ Add an option for custom editor command for *New File* entry
++ Find the correct path to the gensystray-gen executable
